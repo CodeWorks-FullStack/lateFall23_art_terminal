c36d14fe674bb61ed55e2b786e28080c98ad99e0
Jeremy <jembyfowler@gmail.com>
2024-01-11 10:00:13 -0700

    rendered components for project

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <img :src="project.coverImg" class="card-img-top" :alt="project.title">
+    <div class="card-body">
+      <p class="card-title fs-5">{{ project.title }}</p>
+      <button type="button" class="btn btn-danger">Open Gallery</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { Project } from '../models/Project.js';
+
+export default {
+  props: {
+    project: { type: Project, required: true }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,13 +7,7 @@
     </section>
     <section class="row">
       <div v-for="project in projects" :key="project.id" class="col-12 col-md-3">
-        <div class="card">
-          <img :src="project.coverImg" class="card-img-top" :alt="project.title">
-          <div class="card-body">
-            <p class="card-title fs-5">{{ project.title }}</p>
-            <button type="button" class="btn btn-danger">Open Gallery</button>
-          </div>
-        </div>
+        <ProjectCard :project="project" />
       </div>
     </section>
   </div>
@@ -24,25 +18,26 @@ import Pop from '../utils/Pop.js';
 import { projectsService } from '../services/ProjectsService.js'
 import { computed, onMounted } from 'vue';
 import { AppState } from '../AppState.js'
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
   setup() {
     async function getProjects() {
       try {
-        await projectsService.getProjects()
-      } catch (error) {
-        Pop.error(error)
+        await projectsService.getProjects();
+      }
+      catch (error) {
+        Pop.error(error);
       }
     }
-
     onMounted(() => {
-      getProjects()
-    })
-
+      getProjects();
+    });
     return {
       projects: computed(() => AppState.projects)
-    }
-  }
+    };
+  },
+  components: { ProjectCard }
 }
 </script>
 
